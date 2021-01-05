3256af15bd6ce2dc00d44ccee0b1519120c08316
David F. Nettleton <dnettlet@yahoo.es>
2021-01-05 23:40:15 +0100

    Update GenDataNO.java

--- src/genDataNOapplication/GenDataNO.java
+++ src/genDataNOapplication/GenDataNO.java
@@ -3,13 +3,13 @@ package genDataNOapplication;
 import genDataNOapplication.RV.RV;
 import genDataNOapplication.model.ConfigurationModel;
 
-// Copyright (C) 2018  David F. Nettleton (david.nettleton@upf.edu)
+// Copyright (C) 2018-2021  David F. Nettleton (david.nettleton@upf.edu)
 // License: GNU GENERAL PUBLIC LICENSE v3.0   See LICENSE for the full license.
 
 /**
  * @author  David Nettleton
  * @created Nov 28th, 2012
- * @last modified 	April 30th, 2018
+ * @last modified 	Jan 5th, 2021
  */
 public class GenDataNO
 {
