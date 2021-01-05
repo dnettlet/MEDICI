f0b504215542d9ae3eebf3d5e1330d0e1b02d985
David F. Nettleton <dnettlet@yahoo.es>
2021-01-05 23:27:48 +0100

    Add files via upload

---
+++ src/genDataNOapplication/Dijkstra/AssignSeeds2.java
@@ -0,0 +1,419 @@
+// Copyright (C) 2018  David F. Nettleton (david.nettleton@upf.edu)
+// License: GNU GENERAL PUBLIC LICENSE v3.0   See LICENSE for the full license.
+
+package genDataNOapplication.Dijkstra;
+import java.util.PriorityQueue;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Hashtable;
+//import java.util.Random;
+import java.util.Vector;
+
+import genDataNOapplication.RV.RV;
+import genDataNOapplication.User.User;
+import genDataNOapplication.community.Community;
+
+public class AssignSeeds2{
+
+
+public int AssignSeeds2(Hashtable Users, int maxid, int seedsize) 
+{
+	Enumeration en1 = RV.Communities.keys();
+	Community co;
+	
+    int ids=0;
+    
+	RV.seeds.clear();		RV.invalids.clear();
+	
+	co = (Community)RV.Communities.get((Integer)en1.nextElement());
+	int co1 = co.getCommunity();
+	
+    
+    //OBTAIN THE FIRST SEED from community zero
+	ids = obtainseed(co); // this is the node id from the medoid vector
+
+	if (RV.seeds.contains(ids) == false)
+	{
+		RV.seeds.add(ids);
+		int nosa = co.numberofseedassigned;
+		++nosa;
+		co.numberofseedassigned = nosa;
+		RV.Communities.remove(co.getCommunity());
+		RV.Communities.put(co.getCommunity(),co);
+	}
+	else 
+		ids = -1;
+    
+    // ok, got first seed in vector "seeds"
+    
+    int ubercounter=0;
+    int maxtries = 50, tries=0;
+    
+    int besttonow=0;
+    System.out.println("seedsize="+seedsize+" tries="+tries);
+    while ((RV.seeds.size() < seedsize) && (tries < maxtries))  // loop until got 'seedsize' seeds
+    {
+    	//System.out.println("P1");
+		//see the seeds
+		//System.out.print("seedsbesttonow: ");
+		//for(int i=0;i<RV.seedsbesttonow.size();i++)
+			//System.out.print(" "+RV.seedsbesttonow.get(i)+" ");
+		//System.out.println();
+    	if (tries == (maxtries -1))
+    	{
+    		--seedsize;
+    		tries = 0;
+    	}
+    	//System.out.println("seedsize="+seedsize+" tries="+tries+" seeds assigned="+RV.seeds.size());
+    	
+    	if (RV.seeds.size() > besttonow)
+    	{
+    		besttonow = RV.seeds.size();
+    		RV.seedsbesttonow.clear();
+    		for (int i=0;i<RV.seeds.size();i++)
+    			RV.seedsbesttonow.add(i, RV.seeds.get(i));
+    	}
+    	
+    	if (RV.seedsbesttonow.size() >= seedsize) //stop
+    	{
+    		RV.seeds.clear();
+    		for (int i=0;i<RV.seedsbesttonow.size();i++)
+    			RV.seeds.add(i, RV.seedsbesttonow.get(i));
+    		return 0;
+    	}
+    		
+    	++ubercounter;
+    	if (ubercounter > (maxid * 2)) // start again
+    	{
+    		ubercounter = 0;
+    		
+    		RV.seeds.clear();     		RV.invalids.clear();
+    		++tries;
+    		ids = -1;
+    		
+    		//Need to reinitialize the community seed counters
+    		Enumeration enc = RV.Communities.keys();
+    		while(enc.hasMoreElements())
+    		{
+        		co = (Community)RV.Communities.get((Integer)enc.nextElement());
+   
+        		co.numberofseedassigned = 0;
+        		RV.Communities.remove(co.getCommunity());
+        		RV.Communities.put(co.getCommunity(),co);
+    		}
+
+    		//OBTAIN THE FIRST SEED from community zero
+    		en1 = RV.Communities.keys();
+    		co = (Community)RV.Communities.get((Integer)en1.nextElement());
+    		co1 = co.getCommunity();
+    		ids = obtainseed(co); // this is the node id from the medoid vector
+
+    		
+    		if (RV.seeds.contains(ids) == false)
+    		{
+    			RV.seeds.add(ids);
+    			int nosa = co.numberofseedassigned;
+    			++nosa;
+    			co.numberofseedassigned = nosa;
+				RV.Communities.remove(co.getCommunity());
+				RV.Communities.put(co.getCommunity(),co);
+    		}
+    		else 
+    			ids = -1;
+      	}
+        ids = -1;
+        
+    	//System.out.println("P2");
+    	
+        //get THE next SEED for the next community
+		if ( en1.hasMoreElements() )
+			co = (Community)RV.Communities.get((Integer)en1.nextElement());
+		else
+		{
+    		en1 = RV.Communities.keys();
+			co = (Community)RV.Communities.get((Integer)en1.nextElement());
+		}
+   		ids = obtainseed(co); // this is the node id from the medoid vector
+   		
+ 		//System.out.println("node ids returned from obtainseed for community "+co1+" is="+ids);
+    	
+		if ((RV.seeds.contains(ids) == true) || (RV.invalids.contains(ids) == true))
+		{
+			ids = -1;
+		}
+		
+    	//System.out.println("P3");
+    	
+		/*System.out.println("ids is="+ids);
+		System.out.print("seeds: ");
+		for(int i=0;i<RV.seeds.size();i++)
+			System.out.print(" "+RV.seeds.get(i)+" ");
+		System.out.println();
+		System.out.print("invalids: ");
+		for(int i=0;i<RV.invalids.size();i++)
+			System.out.print(" "+RV.invalids.get(i)+" ");
+		System.out.println();*/
+ 
+        // got a candidate seed, now check if its at at least distance 3 from any other seed
+
+ 		//System.out.println("node to check is="+current.name);
+    	if (ids != -1)
+    	{
+        	//System.out.println("P4");
+    		//System.out.println("node="+ids);
+    		int ok = check(ids);
+    		//System.out.println("ok="+ok);
+
+    		// if ids was at least distance three from any other seed, then add it as a seed
+    		if (ok == 0)
+    		{
+    			RV.seeds.add((Integer)ids);
+    			int nosa = co.numberofseedassigned;
+    			++nosa;
+    			co.numberofseedassigned = nosa;
+				RV.Communities.remove(co.getCommunity());
+				RV.Communities.put(co.getCommunity(),co);
+    			// assign same community as seed to 70% of neighbors
+    			//assigncommunitytoneighbours(ids);
+    			
+        		//System.out.println("hola1");
+    		}
+    		else 
+    		{
+    			boolean alreadyinvalid = RV.invalids.contains((Integer)ids);
+        		//System.out.println("hola2: "+alreadyinvalid);
+    			
+    			if (alreadyinvalid == false)
+        			RV.invalids.add((Integer)ids);
+    			/*System.out.print("invalids: ");
+    			for(int i=0;i<RV.invalids.size();i++)
+    				System.out.print(" "+RV.invalids.get(i)+" ");
+    			System.out.println();*/
+    		}
+        	//System.out.println("P5");
+    		
+    	}
+    	//System.out.println("P99");
+
+    }
+    //for (int i=0; i<RV.seeds.size();++i)
+		//System.out.println("seed "+i+" is "+RV.seeds.get(i));
+
+return 0;
+} //fin de AssignSeeds
+
+public static int obtainseed(Community co)
+{
+	int i=0, id=0;
+	boolean notfree=false, invalid=false;
+
+	int co1 = co.getCommunity();
+
+	float best = 0, auth = 0;
+	int bestid=-1;
+	for (i=0;(i<co.vertexids.size()) && (bestid == -1);i++)
+	{
+		id = (Integer)co.vertexids.get(i);
+		
+		notfree = RV.seeds.contains(id);
+		invalid = RV.invalids.contains(id);
+		
+		if((notfree == false) && (invalid == false))
+			bestid = id;
+	}
+    return(bestid); //returns the index or -1
+} // fin check
+
+public static int check(int candidateseedid)
+{
+	int i=0, ret = 0, seedid = 0;
+	//System.out.println("PC1");
+	for (i=0; (i < RV.seeds.size()) && (ret == 0); i++)
+	{
+		//System.out.println("PC2");
+		seedid = (Integer)RV.seeds.get(i);
+		ret = distm3(seedid, candidateseedid);
+		//System.out.println("matched seed id: "+si+" with candidate seed id: "+sx+" and it gave a return code of: "+ret);
+	}
+	//System.out.println("PC99");
+
+ 
+    return(ret);
+} // fin computePaths
+
+public static int distm(int seedid, int candidateseedid)
+{
+	User nw1=null, nw2=null, nw3=null;;
+	int j=0, k=0, ret = 0, sij = 0, sik = 0;
+	
+	nw3 = (User)RV.Users.get(candidateseedid);
+	
+	if (RV.Users.containsKey(seedid) != false)
+	{
+		nw1 = (User)RV.Users.get(seedid); // this is an existing seed
+
+		for (j=0; (j < nw1.friends.size()) && (ret == 0); j++)  // seeds neighbors
+		{
+			sij = nw1.friends.get(j);
+			nw2 = (User)RV.Users.get(sij); // this is the seed
+			
+			if (sij == candidateseedid)
+				ret = -1;
+			for (k=0; (k < nw3.friends.size()) && (ret == 0); k++)  // candidate seeds neighbors
+			{
+				sik = nw3.friends.get(k);
+				if (sij == sik)
+					ret = -1;
+			}
+
+		} //efor
+				
+	} // eif
+	
+    return(ret);
+} // fin distm
+
+public static int distm3(int seedid, int candidateseedid)
+{
+	User nw1=null, nw2=null;
+	int j=0, ret = 0, sij = 0;
+	
+	if (RV.Users.containsKey(seedid) != false) // check that its not a neighbor
+	{
+		nw1 = (User)RV.Users.get(seedid); // this is an existing seed
+		//int com1 = nw1.getMOD();
+		double notin=0.0, totf=0.0;
+	
+		for (j=0, notin=0.0; (j < nw1.friends.size()) && (ret == 0); j++)  // seeds neighbors
+		{
+			sij = nw1.friends.get(j);
+			
+			if (sij == candidateseedid) //cant be an immediate neighbor
+				return -1;
+		} //efor
+	} // eif
+	
+	// Now check that max 30% of neighbors outside community
+	/*if (RV.Users.containsKey(candidateseedid) != false)
+	{
+		nw1 = (User)RV.Users.get(candidateseedid);
+		int com1 = nw1.getMOD();
+		double notin=0.0, totf=0.0;
+	
+		for (j=0, notin=0.0; (j < nw1.friends.size()) && (ret == 0); j++)  // neighbors
+		{
+			sij = nw1.friends.get(j);
+			
+			nw2 = (User)RV.Users.get(sij); // this is an existing seed neighbor
+			int com2 = nw2.getMOD();
+
+			
+			if (com1 != com2)
+				++notin;
+		} //efor
+		totf=nw1.friends.size();
+		notin = notin/totf;
+		if (notin > 0.50)  // only candidates with at least 50% of their neighbors in the same community.
+			return -1;
+	} // eif
+	*/
+	
+	// Now check that max 30% of neighbors overlap between actual seed and candidate seed
+	/*if (RV.Users.containsKey(candidateseedid) != false)
+	{
+		nw1 = (User)RV.Users.get(candidateseedid);
+		nw2 = (User)RV.Users.get(seedid);
+		double notin=0.0, in=0.0, totf=0.0;
+		
+		for (j=0, notin=0.0; (j < nw1.friends.size()) && (ret == 0); j++)  // neighbors
+		{
+			sij = nw1.friends.get(j);
+			
+            if(nw2.friends.contains(sij) == true)
+				++in;
+		} //efor
+		totf=nw2.friends.size();
+		in = in/totf;
+		if (in > 0.50)  // only candidates with at most 50% overlap of neighbors with existing seed
+			return -1;
+	} // eif
+	*/
+	
+	
+    return(ret);
+} // fin distm
+
+public static int distm2(int seedid, int candidateseedid)
+{
+	User nw1=null, nw2=null, nw3=null;;
+	int j=0, k=0, ret = 0, sij = 0, sik = 0;
+	
+	nw3 = (User)RV.Users.get(candidateseedid);
+	
+	if (RV.Users.containsKey(seedid) != false)
+	{
+		nw1 = (User)RV.Users.get(seedid); // this is an existing seed
+
+		for (j=0; (j < nw1.friends.size()) && (ret == 0); j++)  // seeds neighbors
+		{
+			sij = nw1.friends.get(j);
+			nw2 = (User)RV.Users.get(sij); // this is the seed
+			
+			if (sij == candidateseedid)
+				ret = -1;
+		} //efor
+				
+	} // eif
+	
+    return(ret);
+}
+
+
+public static void assigncommunitytoneighbours(int idseed)
+{
+	User nw1=null, nw2=null;
+	int j=0, ret = 0, idseedneighbour = 0;
+	
+	if (RV.Users.containsKey(idseed) != false)
+	{
+		nw1 = (User)RV.Users.get(idseed); // this is the seed
+		int com1 = nw1.getMOD();
+		
+		double numf = nw1.friends.size();
+		double ixf = 0;
+		
+		for (j=0; j < nw1.friends.size(); j++)  // first count how many neighbours are NOT in same community as seed
+		{
+			idseedneighbour = nw1.friends.get(j);
+			nw2 = (User)RV.Users.get(idseedneighbour); // this is the seed
+			int com2 = nw2.getMOD();
+			
+			if (com1 != com2)
+               ++ixf;
+		} //efor
+		
+		ixf = ixf * 0.7; // this is the number to assign when community not same
+		double ixfdone = 0.0;
+	
+		for (j=0; j < nw1.friends.size(); j++)  // seeds neighbors
+		{
+			idseedneighbour = nw1.friends.get(j);
+			nw2 = (User)RV.Users.get(idseedneighbour); // this is the seed
+			int com2 = nw2.getMOD();
+			
+			if ((com1 != com2) && (ixfdone < ixf))
+			{
+               nw2.setMOD(com1);
+			   ++ixfdone;
+			}
+
+		} //efor
+		//System.out.println("friends: "+nw1.friends.size()+" diff community: "+ixf+" done: "+ixfdone);
+		
+		
+	} // eif
+} //end assigncommunitytoneighbours
+
+} // fin de clase AssignSeeds
--- src/genDataNOapplication/Dijkstra/FindMedoid.java
+++ src/genDataNOapplication/Dijkstra/FindMedoid.java
@@ -1,1 +1,184 @@
+// Copyright (C) 2018  David F. Nettleton (david.nettleton@upf.edu)
+// License: GNU GENERAL PUBLIC LICENSE v3.0   See LICENSE for the full license.
 
+package genDataNOapplication.Dijkstra;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Collections;
+import java.util.Vector;
+
+import genDataNOapplication.RV.RV;
+import genDataNOapplication.User.User;
+import genDataNOapplication.community.Community;
+
+public class FindMedoid{
+	private double simid=0;
+	private int id=0;
+		
+	public static Vector vertexidsO      = new Vector ();
+	public static Vector vertexidvaluesO = new Vector ();
+		
+	public int getid()
+	{
+		return id;
+	};
+	public double getsim()
+	{
+		return simid;
+	};
+	
+	private static double alpha=2.8, beta=0.7, gamma=2.0, delta=0.003, epsilon=-0.3;
+	
+	public static double calcdist(User us1, double normDegree1, User us2)
+	{
+		double dist=0;
+		double normDegree2=0;
+		
+		normDegree2       = us2.getNormDegree();
+		
+		dist = (Math.abs(normDegree1 - normDegree2));
+		
+		return(dist);
+	}
+	
+
+public static void FindMedoid(Community co, int com) // finds the medoid of community 'com'
+{
+
+	User nw1, nw2;
+	
+	double dist=99999999;
+	int us1=0,us2=0;
+	
+	int umax=0, imax=0; double dmax=0.0;
+	int umin=0, imin=0; double dmin=0.0;
+	
+	double normDegree1=0, normCC1=0, normAuth1=0;
+	double normDegree2=0, normCC2=0, normAuth2=0;
+	
+	double minsumdist=999999; int minsumdistid=-1;
+		
+		double distattr=0.0;
+		
+		/**************************************************************************************************/
+		/************* Find the vertex with minimum sum of distances to all other vertices   **************/
+		/**************************************************************************************************/
+		nw1 = new User(1);
+		nw2 = new User(1);
+		float deg=0;
+		int i=0, j=0;
+		double sumdist = 0.0;
+
+		int numusersincommunity = co.vertexids.size();
+		
+		//System.out.println("kgroupsize="+kgroupsize);
+		int degg=0;
+		if (numusersincommunity > 0)
+		{
+		
+		for (i=0;i<numusersincommunity;++i){    // outer loop
+			
+			us1 = (Integer)co.vertexids.get(i);
+			nw1 = (User)RV.Users.get(us1);
+			
+			//degg = degg + nw1.friends.size();
+		
+			normDegree1       = nw1.getNormDegree();
+
+			
+			sumdist = 0;
+			
+			for (j=0;j<numusersincommunity;++j){    // inner loop
+				
+				us2 = (Integer)co.vertexids.get(j);
+				nw2 = (User)RV.Users.get(us2);
+	
+				if (us1 != us2 && nw1 != null && nw2!=null) 
+				{
+					// First the topological distance
+					dist = calcdist(nw1, normDegree1, nw2);
+				
+					//System.out.println("CHECKPOINT.CHECKPOINT.CHECKPOINT. Distance between "+us1+" and "+us2+" top: "+dist+" attr: "+distattr);
+				
+					sumdist = sumdist + dist;
+
+
+				} // eif us1 != us2
+			} // efor inner
+			
+			co.vertexidvalues.set(i,(Double)sumdist);
+			
+			
+		} // efor outer
+		} // eif kgroupsize > 0
+		
+		// NOW ORDER THEM......
+		
+		//if(1==1)return;
+		
+		double smallestdistance = 99999.9; int smallestid = -1; int smallestix = -1;
+		double usv1 = 0.0;
+		
+		while (co.vertexids.size() > 0)
+		{
+		
+			numusersincommunity = co.vertexids.size();
+		
+			smallestdistance = 99999.9; smallestid = -1; smallestix = -1;
+			usv1 = 0.0;
+		
+			for (i=0;i<numusersincommunity;++i){    // take a look
+			
+				us1  = (Integer)co.vertexids.get(i);
+				usv1 = (Double)co.vertexidvalues.get(i);
+				if (usv1 < smallestdistance)
+				{
+					smallestdistance = usv1;
+					smallestid = us1;
+					smallestix = i;
+				}
+
+			}
+			// got a smallest distance, now add it to the temporary vector
+		
+			vertexidsO.add(smallestid);
+			vertexidvaluesO.add(smallestdistance);
+			co.vertexids.remove(smallestix);
+			co.vertexidvalues.remove(smallestix);
+			RV.Communities.remove(co.getCommunity());
+			RV.Communities.put(co.getCommunity(),co);
+		
+		} // ewhile vector not empty
+		
+		
+		
+		// Now load the ordered values back into the community structure
+		co.vertexids.clear();
+		co.vertexidvalues.clear();
+		
+		RV.Communities.remove(co.getCommunity());
+		RV.Communities.put(co.getCommunity(),co);
+		
+		numusersincommunity = vertexidsO.size();
+		int id=0; double distid = 0.0;
+		for (i=0;i<numusersincommunity;++i)
+		{
+			id = (Integer)vertexidsO.get(i);
+			distid = (Double)vertexidvaluesO.get(i);
+			co.vertexids.add(id);
+			co.vertexidvalues.add(distid);
+			RV.Communities.remove(co.getCommunity());
+			RV.Communities.put(co.getCommunity(),co);
+		}
+			
+		vertexidsO.clear();
+		vertexidvaluesO.clear();
+		
+		RV.Communities.remove(co.getCommunity());
+		RV.Communities.put(co.getCommunity(),co);
+		
+
+} //end of FindMedoid
+
+} // end of class FindMedoid
