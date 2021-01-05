6097bf8823ce6da6531137cfc3b7cf496bb3aac0
David F. Nettleton <dnettlet@yahoo.es>
2021-01-05 23:23:32 +0100

    Add files via upload

--- src/rMAT/GraphOutput.java
+++ src/rMAT/GraphOutput.java
@@ -1,1 +1,11 @@
+package rMAT;
+/**
+ * @author Aapo Kyrola, akyrola@cs.cmu.edu, akyrola@twitter.com
+ */
+public interface GraphOutput {
 
+    void addEdges(int[] from, int[] to);
+
+    void finishUp();
+
+}
